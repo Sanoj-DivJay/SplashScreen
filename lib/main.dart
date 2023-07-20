9f986c14bc5c66cdb08518ca2fe4d30866f379dd
Sanoj Divakara <sanojdivjay@gmail.com>
2023-07-20 11:46:14 +0530

    Splash Screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'splash_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your App',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const Splash(), // Set the SplashPage as the home page
+    );
+  }
+}
